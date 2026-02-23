ff80f1a9ff0e91f82acd182d19b8c80a84f11cad
vs9842 <vs9842@srmist.edu.in>
2026-02-23 09:47:08 +0530

    Coded UC3 to print OOPS in asterisks using the string.join() method in java

--- UC3_OOPSBannerApp.java
+++ UC3_OOPSBannerApp.java
@@ -10,15 +10,6 @@
 
 public class UC3_OOPSBannerApp{
 	public static void main(String[] args){
-		System.out.println("   ***       ***    *******       *****  ");
-		System.out.println(" **   **   **   **  **     **  	** 	     ");
-		System.out.println("**     ** **     ** **      ** **        ");
-		System.out.println("**     ** **     ** **     **   **       ");
-		System.out.println("**     ** **     ** *******       ***    ");
-		System.out.println("**     ** **     ** **               **  "); 
-		System.out.println("**     ** **     ** **                ** ");
-		System.out.println(" **   **   **   **  **               **  ");
-		System.out.println("   ***       ***    **          *****  ");
 		System.out.println(String.join(" ","   ***   ","   ***   ",	"*******   ", "   *****  "));
 		System.out.println(String.join(" "," **   ** "," **   ** ", "**     ** ", "	** 	     "));
 		System.out.println(String.join(" ","**     **","**     **", "**      **", "**        "));
