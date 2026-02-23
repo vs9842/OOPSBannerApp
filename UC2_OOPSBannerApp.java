02cd9a243e8824ed20fea47d8a6a926663ed0f7f
vs9842 <vs9842@srmist.edu.in>
2026-02-23 08:49:12 +0530

    Coded UC2 to print OOPS in Asterisks

---
+++ UC2_OOPSBannerApp.java
@@ -0,0 +1,21 @@
+/**
+* OOPSBannerApp UC2 - OOPS Banner Display Application
+* 
+* This class displays a simple java application that displays the Object 
+* Oriented Programming System OOPS acronym to the console
+*
+* @vs9842 Developer
+* @version 1.0
+*/
+
+public class UC2_OOPSBannerApp{
+	public static void main(String[] args){
+		System.out.println(" *****    *****   *****   *****");
+		System.out.println("*     *  *     *  *    *  *     ");
+		System.out.println("*     *  *     *  *    *  *     ");
+		System.out.println("*     *  *     *  *****   *****  ");
+		System.out.println("*     *  *     *  *           *  ");
+		System.out.println("*     *  *     *  *           * ");
+		System.out.println(" *****    *****   *       *****  ");
+	}
+}
